867c4d887af47c5aa31c24042299475dc4926e97
shivani26shinde <shindeshivani26@gmail.com>
2017-06-11 23:01:13 -0500

    Subset

--- BackTracking/subsets_set.java
+++ BackTracking/subsets_set.java
@@ -1,38 +1,27 @@
 # Subsets of a set
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Ideone
-{
-	static Set<ArrayList<Integer>> set = new HashSet<>();
-	static int[] array = new int[]{1,2,3};
-	public static void Subset(int a){
-		if(a<=array.length){
-			ArrayList<Integer> list = new ArrayList<>();
-			for(int j=a;j<array.length;j++){
-				list.add(array[j]);
-			}
-			Collections.sort(list);
-			if(!set.contains(list)){
-				set.add(list);
-			}
-		}
-		for(int j=a;j<array.length;j++){
-			swap(a, j);
-			Subset(a+1);
-			swap(a, j);
-		}
+public class Solution {
+    ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    HashSet<ArrayList<Integer>> set = new HashSet<>();
+	public ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> a) {
+	    Set<Integer> s = new HashSet<>(a);
+	    ArrayList<Integer> sorted = new ArrayList<Integer>(s);
+	    Collections.sort(sorted);
+	    Subset(0,new ArrayList(),sorted);
+	    return list;
 	}
-	public static void swap(int a, int b){
-		int temp = array[a];
-		array[a] = array[b];
-		array[b] = temp;
-	}
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Subset(0);
-		System.out.println(Arrays.toString(Flist.toArray()));
+	public void Subset(int start,ArrayList<Integer> temp,ArrayList<Integer> a){
+	    if(start <= a.size()){
+	        ArrayList<Integer> t = new ArrayList(temp);
+	        if(!set.contains(t)){
+	            list.add(t);
+	            set.add(t);   
+	        }
+	    }
+	    for(int i=start;i<a.size();i++){
+	        temp.add(a.get(i));
+	        Subset(i+1,temp,a);
+	        temp.remove(temp.size()-1);
+	    }
 	}
 }
