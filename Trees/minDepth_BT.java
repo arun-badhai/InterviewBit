87d584e0dbb97786df1c141347e86ffcc23db01f
shivani26shinde <shindeshivani26@gmail.com>
2017-06-16 00:36:06 -0500

    Minimum depth in a BT

---
+++ Trees/minDepth_BT.java
@@ -0,0 +1,28 @@
+# Min Depth
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	public int minDepth(TreeNode a) {
+	    if(a == null){
+	        return 0;
+	    }
+	    if(a.left == null && a.right == null){
+	        return 1;
+	    }
+	    if(a.left == null){
+	        return minDepth(a.right)+1;
+	    }
+	    if(a.right == null){
+	        return minDepth(a.left)+1;
+	    }
+	    return Math.min(minDepth(a.right), minDepth(a.left))+1;
+	}
+}
