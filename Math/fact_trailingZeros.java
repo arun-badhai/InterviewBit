e6a48593cad18c59935155d4b0211e6f952ac01c
shivani26shinde <shindeshivani26@gmail.com>
2017-07-10 22:20:22 -0500

    Update

---
+++ Math/fact_trailingZeros.java
@@ -0,0 +1,13 @@
+# Trailing 0's in factorial
+
+public class Solution {
+  
+	public int trailingZeroes(int a) {
+	    int count = 0;
+	     while(a != 0){
+	         count = count + a/5;
+	         a = a/5;
+	     }
+	    return count;
+	}
+}
